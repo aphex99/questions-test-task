84e413399c2d5ee0e300beb26a95c3a5e58e1d65
belovroman90 <belov.roman.90@mail.ru>
2026-07-14 11:14:09 +0300

    refactor: change git settings, add dependencies and FSD structure

---
+++ eslint.config.mjs
@@ -0,0 +1,101 @@
+import js from "@eslint/js";
+import boundaries from "eslint-plugin-boundaries";
+import eslintPluginImport from "eslint-plugin-import";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  {
+    files: ["**/*.js", "**/*.mjs", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+
+    ignores: ["build/**", "node_modules/**"],
+
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        myCustomGlobal: "readonly",
+      },
+    },
+
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+      import: eslintPluginImport,
+      boundaries,
+    },
+
+    settings: {
+      "boundaries/elements": [
+        { type: "app", pattern: "src/app/*" },
+        { type: "pages", pattern: "src/pages/*" },
+        { type: "widgets", pattern: "src/widgets/*" },
+        { type: "features", pattern: "src/features/*" },
+        { type: "entities", pattern: "src/entities/*" },
+        { type: "shared", pattern: "src/shared/*" },
+      ],
+    },
+
+    rules: {
+      "simple-import-sort/exports": "error",
+      "no-unused-vars": "off",
+      "no-console": "warn",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      eqeqeq: "error",
+      curly: "error",
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            ["^react", "react-dom$"],
+
+            // third-party
+            ["^@?\\w"],
+            ["^@/app(/.*|$)"],
+            ["^@/pages(/.*|$)"],
+            ["^@/widgets(/.*|$)"],
+            ["^@/features(/.*|$)"],
+            ["^@/entities(/.*|$)"],
+            ["^@/shared(/.*|$)"],
+
+            // side effects
+            ["^\\u0000"],
+
+            // parent imports
+            ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+
+            // same-folder
+            ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+
+            // styles
+            ["^.+\\.s?css$"],
+          ],
+        },
+      ],
+      "boundaries/element-types": [
+        "error",
+        {
+          default: "disallow",
+          rules: [
+            { from: "app", allow: ["pages", "widgets", "features", "entities", "shared"] },
+
+            { from: "pages", allow: ["widgets", "features", "entities", "shared"] },
+
+            { from: "widgets", allow: ["features", "entities", "shared"] },
+
+            { from: "features", allow: ["entities", "shared"] },
+
+            { from: "entities", allow: ["shared"] },
+
+            { from: "shared", allow: ["shared"] },
+          ],
+        },
+      ],
+    },
+  },
+];
---
+++ src/app/App.tsx
@@ -0,0 +1,15 @@
+import Questions from "@/pages/questions/Questions.tsx";
+
+import Button from "@/shared/ui/button/Button.tsx";
+
+function App() {
+  return (
+    <>
+      <Questions />
+      <Button />
+      <div id={"don"}></div>
+    </>
+  );
+}
+
+export default App;
