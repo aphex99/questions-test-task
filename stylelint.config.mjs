62b42aa30262b9cee86b8ca6762922d4b937647c
belovroman90 <belov.roman.90@mail.ru>
2026-07-14 18:20:45 +0300

    refactor: format styles

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -4,5 +4,9 @@ export default {
   plugins: ["@stylistic/stylelint-plugin"],
   rules: {
     "@stylistic/color-hex-case": "lower",
+    "selector-class-pattern": [
+      "^[a-z][a-zA-Z0-9]*$",
+      { message: "Expected class selector to be camelCase" },
+    ],
   },
 };
